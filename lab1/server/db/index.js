d7f1ec5796d9a8b0af092eb31d27ef599400b63c
Luka <luka.slugecic@gmail.com>
2023-10-29 14:17:32 +0100

    db url

--- lab1/server/db/index.js
+++ lab1/server/db/index.js
@@ -12,7 +12,7 @@ const pool = new Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    connectionString: isProduction ? process.env.DB_URL : connectionString,
     ssl: isProduction
 });
 
