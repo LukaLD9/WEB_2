d26434571e313e0d9e2265c33183fdbea8b462a9
Luka <luka.slugecic@gmail.com>
2023-10-29 13:42:10 +0100

    connectionstring

--- lab1/server/db/index.js
+++ lab1/server/db/index.js
@@ -2,13 +2,18 @@ const {Pool} = require('pg');
 var dotenv = require('dotenv');
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    ssl: true
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: isProduction
 });
 
 dangerous_query = async (text, params) => {
