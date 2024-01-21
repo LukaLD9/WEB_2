1ce7aec0b43685757e2f6711c672ae6b28b31f6a
Luka <luka.slugecic@gmail.com>
2024-01-21 17:34:03 +0100

    app made

---
+++ lab6/lab6-vue-app/src/views/NotFoundView.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Sorry, {{ $route.params.catchAll }} Chuck doesnt't have this page.</h1>
+    Try <router-link to="/">home.</router-link>
+    <br />
+    <img src="@/assets/chuck2.png" />
+</template>
+  
